3cdad9bba35594cb2819a63916f2db7ddb772bc6
Kafaa Billahi Syahida <kafaa.id@proton.me>
2025-01-24 23:46:58 +0700

    feat: install home manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
 
     nixosConfigurations.nixos-mint = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.kafaa = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
     };
 
   };
