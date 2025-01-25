918083e40e8eab439a7c2e8558165ad59d349e96
Kafaa Billahi Syahida <kafaa.id@proton.me>
2025-01-26 01:43:08 +0700

    feat(home,fish): install Laravel Artisan command completion

--- home.nix
+++ home.nix
@@ -64,6 +64,18 @@
     shellAliases = {
       ls = "eza";
     };
+
+    plugins = [
+      {
+        name = "artisan";
+        src = pkgs.fetchFromGitHub {
+          owner = "adriaanzon";
+          repo = "fish-artisan-completion";
+          rev = "8e8d726b3862fcb972abb652fb8c1a9fb9207a64";
+          sha256 = "1vjyv42q6slgspw4hjnvi9bmn6pagiq6gn5zp0r9fi4fwmb91cpq";
+        };
+      }
+    ];
   };
 
   programs.starship = {
