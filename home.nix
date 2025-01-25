717ab59502c6ad51e987dc417bc95ffea6a20e0c
Kafaa Billahi Syahida <kafaa.id@proton.me>
2025-01-25 17:00:37 +0700

    feat(home,git): sign using my 2025 GPG key

--- home.nix
+++ home.nix
@@ -43,6 +43,7 @@
     enable = true;
     userName = "Kafaa Billahi Syahida";
     userEmail = "kafaa.id@proton.me";
+    signing.key = "3A4F1BBD6EF9A6B9"; # 2025 - NixOS
     signing.signByDefault = true;
     extraConfig = {
       init.defaultBranch = "main";
