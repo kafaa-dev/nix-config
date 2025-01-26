ad5659296d1718df06163a7b64249a3de51136a4
Kafaa Billahi Syahida <kafaa.id@proton.me>
2025-01-26 14:46:22 +0700

    feat: configure Neovim with Home Manager

--- home/editors/neovim/default.nix
+++ home/editors/neovim/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
-{
+let
+  configPath = "${config.home.homeDirectory}/Repos/kafaa-dev/nix-config/home/editors/neovim/nvim";
+in {
+  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink configPath;
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
