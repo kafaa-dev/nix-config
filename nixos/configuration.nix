8e94181615740e674accc32bf4f69768575a2025
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2025-01-27 12:54:40 +0700

    feat(nixos): enable VirtualBox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -125,6 +125,9 @@
     nerd-fonts.jetbrains-mono
   ];
 
+  # Virtualization
+  virtualisation.virtualbox.host.enable = true;
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
