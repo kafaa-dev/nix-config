0a2998d39d480eb832cb0b217923f79c705241df
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2025-02-03 21:16:25 +0700

    feat(home): install NixVim to configure Neovim

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
@@ -22,6 +25,9 @@
 
           # Optionally, use home-manager.extraSpecialArgs to pass
           # arguments to home.nix
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+          };
         }
       ];
     };
