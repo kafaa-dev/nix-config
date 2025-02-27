3cf8ce27056cdb97abfe707da696f111d0fa0110
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2025-02-27 23:23:08 +0700

    feat(nixos): uninstall Plasma Browser Integration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,6 +43,10 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+  ];
+
   services.displayManager.defaultSession = "plasmax11";
 
   # Configure keymap in X11
