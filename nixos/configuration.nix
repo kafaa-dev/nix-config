217c08ae9c023a58947ac3e8b0329b5b5e7de631
Kafaa Billahi Syahida <kafaa.id@proton.me>
2025-03-02 19:44:31 +0700

    revert: uninstall KDE Plasma 6 and install Cinnamon
    This reverts commit 6250941ab2f3bfd220c89e64c8b0a162cbf32ddc.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,17 +45,16 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    elisa
-    kate
-    konsole
+  services.displayManager.defaultSession = "cinnamon";
+
+  environment.cinnamon.excludePackages = with pkgs; [
+    blueman
+    celluloid
+    warpinator
   ];
-
-  services.displayManager.defaultSession = "plasmax11";
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
